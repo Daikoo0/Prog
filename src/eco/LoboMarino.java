f4179f7833e1ebac30f9feff24bfbd54116245da
yoyit <yoyit@DESKTOP-H8OI1HI>
2020-12-14 23:05:20 -0400

    xd

--- src/eco/LoboMarino.java
+++ src/eco/LoboMarino.java
@@ -1,14 +1,14 @@
-
 package eco;
 
-
 public class LoboMarino extends Peces {
+    
     public LoboMarino(){
-        Nombre = "Pulpo";
+        Nombre = "LoboMarino";
         Depredar = 3;
         Especie = 3;
         AlimentacionMin = 0;
         AlimentacionMax = 0;
         Reproduccion = 0;
     }
+    
 }
